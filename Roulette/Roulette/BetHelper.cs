3b14b90b228588619cb2cfb349783a2104a00fa6
Jason Horvath <47703915+jayhawk84@users.noreply.github.com>
2020-10-22 09:16:08 -0700

    Roulette Game & Vectors
    This is a cool little console game that simulates the roulette wheel.  My favorite part is towards the end where I used a multidimensional array to create the board and generate all the sets of winning bids.
    
    The Vectorboy is a little console app that will generate random points in both a 2D plane as well as a 3D space, then provide the shortest distance between the closest two points.

---
+++ Roulette/Roulette/BetHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roulette
+{
+    public static class BetHelper
+    {
+        public static void CalculateAndWriteWinningBets(int whichBin)
+        {
+            if (whichBin != 0 && whichBin != 37)
+            {
+                CheckAndWriteWinningBets(whichBin);
+            }
+            else if (whichBin == 37)
+            {
+                Console.WriteLine("00");
+                Console.WriteLine("Better luck next time!");
+            }
+            else
+            {
+                Console.WriteLine("0");
+                Console.WriteLine("Better luck next time!");
+            }
+        }
+
+        public static void CheckAndWriteWinningBets(int whichBin)
+        {
+            Console.WriteLine($"The winning bin Number is {whichBin}\nOther winning bets would be:");
+            Console.WriteLine(BinsToBets.EvenOrOdd(whichBin));
+            Console.WriteLine(BinsToBets.RedOrBlack(whichBin));
+            Console.WriteLine(BinsToBets.LowOrHigh(whichBin));
+            Console.WriteLine(BinsToBets.WhichDozen(whichBin));
+            Console.WriteLine(BinsToBets.Column(whichBin));
+            BinsToBets.Street(whichBin);
+            Console.WriteLine(BinsToBets.WhichDouble(whichBin));
+            Console.WriteLine(BinsToBets.WhichSplit(whichBin));
+            Console.WriteLine(BinsToBets.WhichCorner(whichBin));
+        }
+    }
+}
